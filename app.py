f08ff51da2e394ea393fe8b519132fecd9c38096
lottapispa <lotta.pispa@helsinki.fi>
2023-11-18 22:15:05 +0200

    templates and files in it created + changes to app.py

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from flask import Flask
-
+from flask import render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Heipparallaa!"
+def login():
+    return render_template("login.html")
+
+@app.route("/frontpage", methods=["POST"])
+def front_page():
+    return render_template("frontpage.html", name=request.form["name"])
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login", methods=["POST"])
+def login_after_register():
+    return render_template("login.html", name=request.form["name"])
+
+@app.route("/elementary-math")
+def elementary_math():
+    return render_template("elementary-math.html")
+
+@app.route("/addition")
+def addition():
+    return render_template("addition.html")
+
+@app.route("/el-math-statistics")
+def statistics():
+    return render_template("el-math-statistics.html")
